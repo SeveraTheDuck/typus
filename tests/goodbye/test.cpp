7c0a64fc7e81e4ca5ad22ff5c5497c86e6582386
SeveraTheDuck <severa.dev@gmail.com>
2026-04-27 00:03:05 +0300

    Initial commit from template

--- tests/goodbye/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <project_name/goodbye/goodbye.hpp>
-
-#include <gtest/gtest.h>
-
-TEST(GoodbyeTest, PrintDoesNotCrash) {
-  EXPECT_NO_THROW(goodbye::Print());
-}
--- tests/hello/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <project_name/hello/hello.hpp>
-
-#include <gtest/gtest.h>
-
-TEST(HelloTest, PrintDoesNotCrash) {
-  EXPECT_NO_THROW(hello::Print());
-}
