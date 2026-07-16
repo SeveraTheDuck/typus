e655178c7073e65b03a136cd353d6e124866832b
SeveraTheDuck <severa.dev@gmail.com>
2026-07-16 15:02:44 +0300

    chore: cmake configuration

--- cmake/UserOptions.cmake
+++ cmake/UserOptions.cmake
@@ -12,5 +12,5 @@
 #
 # Leave empty if you have no custom options.
 
-target_compile_options(project_options INTERFACE -freflection)
-target_compile_features(project_options INTERFACE cxx_std_26)
+target_compile_options(${PROJECT_NAME} INTERFACE -freflection)
+target_compile_features(${PROJECT_NAME} INTERFACE cxx_std_26)
